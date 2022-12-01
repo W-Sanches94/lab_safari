0c8290ada5617aee401cebdb06f9f6a28d60d6ab
Berna <btnaberna@Bernas-Macbook-Air.local>
2022-12-01 18:49:07 +0000

    added some text

--- safari_queries.sql
+++ safari_queries.sql
@@ -1,6 +1,8 @@
 
 -- The names of the animals in a given enclosure
 
+-- SELECT animals.name FROM animals inner join enclosures
+-- ON enclosure.id = animals.enclosures_id where enclosures_id =1;
 
-
+-- the above code is telling us the names of the animals in the enclosure with id 1 (aviary)
 
