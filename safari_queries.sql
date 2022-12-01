c5e56e4fc388d3ae8e9fe8bce16bc0d2e1651ecf
W-Sanches94 <117460587+W-Sanches94@users.noreply.github.com>
2022-12-01 19:14:31 +0000

    MVP complete

--- safari_queries.sql
+++ safari_queries.sql
@@ -1,10 +1,13 @@
 
 -- The names of the animals in a given enclosure
 
+SELECT animals.name FROM animals 
+inner join enclosures
+ON enclosures.id = animals.enclosures_id where enclosures_id =1;
 
+--The names of the staff in a given enclosure
 
-
--- SELECT animals.name FROM animals 
--- inner join enclosures
--- ON enclosures.id = animals.enclosures_id where enclosures_id =1;
-
+SELECT employees.name from employees
+INNER JOIN assignments ON employees.id = assignments.employee_id
+INNER JOIN enclosures ON enclosures.id = assignments.enclosures_id
+WHERE enclosures_id =1;
