b4743a238a5ebf6caa1a78151768795b0799963e
Berna <btnaberna@Bernas-Macbook-Air.local>
2022-12-01 19:00:06 +0000

    additions to queries

--- safari_queries.sql
+++ safari_queries.sql
@@ -1,3 +1,10 @@
 
 -- The names of the animals in a given enclosure
 
+
+
+
+-- SELECT animals.name FROM animals 
+-- inner join enclosures
+-- ON enclosures.id = animals.enclosures_id where enclosures_id =1;
+
